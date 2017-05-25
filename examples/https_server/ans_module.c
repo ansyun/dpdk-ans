3b5b21cfd5c339de01e91aa9157aec3b0defe222
bluestar <anssupport@163.com>
2017-05-25 08:34:05 +0800

    Update ans_module.c

--- examples/https_server/ans_module.c
+++ examples/https_server/ans_module.c
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2010-2014 ANS Corporation. All rights reserved.
+ *   Copyright(c) 2015-2017 Ansyun <anssupport@163.com>. All rights reserved.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *       notice, this list of conditions and the following disclaimer in
  *       the documentation and/or other materials provided with the
  *       distribution.
- *     * Neither the name of ANS Corporation nor the names of its
+ *     * Neither the name of Ansyun <anssupport@163.com> nor the names of its
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
  *
@@ -30,7 +30,7 @@
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *   Author: JiaKai (jiakai1000@gmail.com) and Bluestar (zimeiw@163.com)
+ *   Author: JiaKai (jiakai1000@gmail.com) and Bluestar (anssupport@163.com)
  */
 
 #include <stdio.h>
