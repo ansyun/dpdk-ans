8919e3c70e05bbf5a4dc163475be4889d7209da1
bluenet13 <anssupport@163.com>
2018-01-31 19:11:57 +0800

    support knl march

--- install_deps.sh
+++ install_deps.sh
@@ -115,25 +115,25 @@ elif [ $NATIVE_MATCH = "broadwell" ] ; then
     
     echo "Generate librte_ans.a/librte_anssock.a/librte_anscli.a for broadwell successfully."
 
-#elif [ $NATIVE_MATCH = "knl" ] ; then   
+elif [ $NATIVE_MATCH = "knl" ] ; then   
 
-#    cd $RTE_ANS/librte_ans/
-#    rm -f librte_ans.a
-#    ln -s librte_ans_knl.a librte_ans.a
+    cd $RTE_ANS/librte_ans/
+    rm -f librte_ans.a
+    ln -s librte_ans_knl.a librte_ans.a
 
-#    cd $RTE_ANS/librte_anssock/
-#    rm -f librte_anssock.a
-#    ln -s librte_anssock_knl.a librte_anssock.a
+    cd $RTE_ANS/librte_anssock/
+    rm -f librte_anssock.a
+    ln -s librte_anssock_knl.a librte_anssock.a
   
-#    cd $RTE_ANS/librte_anscli/
-#    rm -f librte_anscli.a
+    cd $RTE_ANS/librte_anscli/
+    rm -f librte_anscli.a
 #    ln -s librte_anscli_knl.a librte_anscli.a
     
-#    echo "Generate librte_ans.a/librte_anssock.a/librte_anscli.a for knl successfully."
+    echo "Generate librte_ans.a/librte_anssock.a/librte_anscli.a for knl successfully."
 	
 else 
 
-    echo "Your native march is none of westmere/sandybridge/ivybridge/haswell/broadwell." 
+    echo "Your native march is none of westmere/sandybridge/ivybridge/haswell/broadwell/knl." 
     echo "You may generate librte_ans.a/librte_anssock.a based on your -mnative by manual,"
     echo "Or ask help from ANS develop team." 
     echo "core2:Intel Core 2 CPU with 64-bit extensions, MMX, SSE, SSE2, SSE3 and SSSE3 instruction set support."
@@ -142,7 +142,7 @@ else
     echo "ivybridge:Intel Ivy Bridge CPU with 64-bit extensions, MMX, SSE, SSE2, SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, AVX, AES, PCLMUL, FSGSBASE, RDRND and F16C instruction set support."
     echo "haswell:Intel Haswell CPU with 64-bit extensions, MOVBE, MMX, SSE, SSE2, SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, AVX, AVX2, AES, PCLMUL, FSGSBASE, RDRND, FMA, BMI, BMI2 and F16C instruction set support."
     echo "broadwell:Intel Broadwell CPU with 64-bit extensions, MOVBE, MMX, SSE, SSE2, SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, AVX, AVX2, AES, PCLMUL, FSGSBASE, RDRND, FMA, BMI, BMI2, F16C, RDSEED, ADCX and PREFETCHW instruction set support."
-#    echo "knl:Intel Knight's Landing CPU with 64-bit extensions, MOVBE, MMX, SSE, SSE2, SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, AVX, AVX2, AES, PCLMUL, FSGSBASE, RDRND, FMA, BMI, BMI2, F16C, RDSEED, ADCX, PREFETCHW, AVX512F, AVX512PF, AVX512ER and AVX512CD instruction set support.."
+    echo "knl:Intel Knight's Landing CPU with 64-bit extensions, MOVBE, MMX, SSE, SSE2, SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, AVX, AVX2, AES, PCLMUL, FSGSBASE, RDRND, FMA, BMI, BMI2, F16C, RDSEED, ADCX, PREFETCHW, AVX512F, AVX512PF, AVX512ER and AVX512CD instruction set support.."
 
 fi     
 
